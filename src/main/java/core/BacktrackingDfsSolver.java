e930512e751daaefeefa3e1977fd0d6b2a463def
Emil Englesson <englesson.emil@gmail.com>
2018-04-19 15:12:52 +0200

    Got rid off an ugly break label
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/main/java/core/BacktrackingDfsSolver.java
+++ src/main/java/core/BacktrackingDfsSolver.java
@@ -5,10 +5,11 @@ import static core.SudokuUtils.*;
 public class BacktrackingDfsSolver implements SudokuSolver {
 
 	/**
-	 * A backtracking depth first search algorithm.
-	 * Assumes a NxN matrix with square sub-boxes with side sqrt(N);
+	 * A backtracking depth first search algorithm. Assumes a NxN matrix with
+	 * square sub-boxes with side sqrt(N);
 	 * 
-	 * @param a NxN matrix
+	 * @param a
+	 *            NxN matrix
 	 * 
 	 * @return null if no solution found, or the same matrix that was inputted.
 	 */
@@ -18,21 +19,24 @@ public class BacktrackingDfsSolver implements SudokuSolver {
 		int side = sudoku.length;
 		int boxSide = (int) Math.sqrt(side);
 
-		boolean noEmptyCells = true;
+		boolean hasEmptyCells = false;
 		int currentRow = 0;
 		int currentCol = 0;
 
-		checkForEmpty: for (int row = 0; row < side; row++) {
+		for (int row = 0; row < side; row++) {
 			for (int col = 0; col < side; col++) {
 				if (sudoku[row][col] == 0) {
 					currentRow = row;
 					currentCol = col;
-					noEmptyCells = false;
-					break checkForEmpty;
+					hasEmptyCells = true;
+					break;
 				}
 			}
+			if (hasEmptyCells) {
+				break;
+			}
 		}
-		if (noEmptyCells) {
+		if (!hasEmptyCells) {
 			return sudoku;
 		}
 
