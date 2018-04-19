d33843fcdc654ee4cc324d517b5e653f5219ccd1
Emil Englesson <englesson.emil@gmail.com>
2018-04-19 14:52:32 +0200

    The REPL is now timing the result and SudokuUtils has common useful methods. Refactored the backtracker
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/main/java/core/BacktrackingDfsSolver.java
+++ src/main/java/core/BacktrackingDfsSolver.java
@@ -1,5 +1,7 @@
 package core;
 
+import static core.SudokuUtils.*;
+
 public class BacktrackingDfsSolver implements SudokuSolver {
 
 	/**
@@ -31,7 +33,7 @@ public class BacktrackingDfsSolver implements SudokuSolver {
 		}
 
 		for (int choice = 1; choice <= side; choice++) {
-			boolean isValid = validBox(sudoku, currentRow, currentCol, choice, boxSide)
+			boolean isValid = validForSubBox(sudoku, currentRow, currentCol, choice, boxSide, boxSide)
 					&& validForRow(sudoku, currentRow, choice) && validForCol(sudoku, currentCol, choice);
 
 			if (isValid) {
@@ -47,36 +49,36 @@ public class BacktrackingDfsSolver implements SudokuSolver {
 		return null;
 	}
 
-	private static boolean validBox(int[][] sudoku, int row, int col, int value, int boxSide) {
-		int boxRow = row / boxSide;
-		int boxCol = col / boxSide;
-
-		for (int r = 0; r < boxSide; r++) {
-			for (int c = 0; c < boxSide; c++) {
-				if (sudoku[boxSide * boxRow + r][boxSide * boxCol + c] == value) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-
-	private static boolean validForRow(int[][] sudoku, int row, int value) {
-		for (int col = 0; col < sudoku[row].length; col++) {
-			if (sudoku[row][col] == value) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	private static boolean validForCol(int[][] sudoku, int col, int value) {
-		for (int row = 0; row < sudoku.length; row++) {
-			if (sudoku[row][col] == value) {
-				return false;
-			}
-		}
-		return true;
-	}
+//	private static boolean validBox(int[][] sudoku, int row, int col, int value, int boxSide) {
+//		int boxRow = row / boxSide;
+//		int boxCol = col / boxSide;
+//
+//		for (int r = 0; r < boxSide; r++) {
+//			for (int c = 0; c < boxSide; c++) {
+//				if (sudoku[boxSide * boxRow + r][boxSide * boxCol + c] == value) {
+//					return false;
+//				}
+//			}
+//		}
+//		return true;
+//	}
+//
+//	private static boolean validForRow(int[][] sudoku, int row, int value) {
+//		for (int col = 0; col < sudoku[row].length; col++) {
+//			if (sudoku[row][col] == value) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//
+//	private static boolean validForCol(int[][] sudoku, int col, int value) {
+//		for (int row = 0; row < sudoku.length; row++) {
+//			if (sudoku[row][col] == value) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
 
 }
---
+++ src/main/java/core/SudokuUtils.java
@@ -0,0 +1,126 @@
+package core;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class SudokuUtils {
+
+	private SudokuUtils() {}
+
+	public static boolean isValid(int[][] sudoku, int boxWidth, int boxHeight) {
+		return uniqueRows(sudoku) && uniqueCols(sudoku) && uniqueSubBoxes(sudoku, boxWidth, boxHeight);
+	}
+
+	public static boolean isSolved(int[][] sudoku, int boxWidth, int boxHeight) {
+		if (hasEmptyBoxes(sudoku)) {
+			return false;
+		}
+		return isValid(sudoku, boxWidth, boxHeight);
+	}
+
+	private static boolean hasEmptyBoxes(int[][] sudoku) {
+		for (int row = 0; row < sudoku.length; row++) {
+			for (int col = 0; col < sudoku[row].length; col++) {
+				if (sudoku[row][col] == 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean uniqueRows(int[][] sudoku) {
+		for (int row = 0; row < sudoku.length; row++) {
+			Set<Integer> numbers = new HashSet<>();
+			for (int col = 0; col < sudoku[row].length; col++) {
+				int value = sudoku[row][col];
+				if (value == 0) {
+					continue;
+				}
+				boolean unique = numbers.add(value);
+				if (!unique) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static boolean uniqueCols(int[][] sudoku) {
+		for (int col = 0; col < sudoku.length; col++) {
+			Set<Integer> numbers = new HashSet<>();
+			for (int row = 0; row < sudoku[col].length; row++) {
+				int value = sudoku[row][col];
+				if (value == 0) {
+					continue;
+				}
+				boolean unique = numbers.add(value);
+				if (!unique) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static boolean uniqueSubBoxes(int[][] sudoku, int boxWidth, int boxHeight) {
+		int nbrOfBoxesX = sudoku.length / boxWidth;
+		int nbrOfBoxesY = sudoku[0].length / boxHeight;
+
+		for (int r = 0; r < nbrOfBoxesX; r++) {
+			for (int c = 0; c < nbrOfBoxesY; c++) {
+				uniqueSubBoxAt(sudoku, r * boxWidth, c * boxHeight, boxWidth, boxHeight);
+			}
+		}
+		return true;
+	}
+
+	private static boolean uniqueSubBoxAt(int[][] sudoku, int row, int col, int boxWidth, int boxHeight) {
+		int boxRow = row / boxWidth;
+		int boxCol = col / boxHeight;
+
+		Set<Integer> numbers = new HashSet<>();
+		for (int r = 0; r < boxHeight; r++) {
+			for (int c = 0; c < boxWidth; c++) {
+				boolean unique = numbers.add(sudoku[boxHeight * boxRow + r][boxWidth * boxCol + c]);
+				if (!unique) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static boolean validForSubBox(int[][] sudoku, int row, int col, int value, int boxWidth, int boxHeight) {
+		int boxRow = row / boxWidth;
+		int boxCol = col / boxHeight;
+
+		for (int r = 0; r < boxHeight; r++) {
+			for (int c = 0; c < boxWidth; c++) {
+				if (sudoku[boxHeight * boxRow + r][boxWidth * boxCol + c] == value) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public static boolean validForRow(int[][] sudoku, int row, int value) {
+		for (int col = 0; col < sudoku[row].length; col++) {
+			if (sudoku[row][col] == value) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean validForCol(int[][] sudoku, int col, int value) {
+		for (int row = 0; row < sudoku.length; row++) {
+			if (sudoku[row][col] == value) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
