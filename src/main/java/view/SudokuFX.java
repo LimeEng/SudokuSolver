16ba2e1afee26422c7ea4e677955147b63bc74df
Emil Englesson <englesson.emil@gmail.com>
2018-04-25 22:27:37 +0200

    Removed more ununsed code
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

--- src/main/java/view/SudokuFX.java
+++ src/main/java/view/SudokuFX.java
@@ -1,8 +1,5 @@
 package view;
 
-import java.io.File;
-import java.net.MalformedURLException;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -15,20 +12,8 @@ public class SudokuFX extends Application {
 		SudokuPlay box = new SudokuPlay();
 
 		Scene scene = new Scene(box, 1200, 900);
-		scene.getStylesheets()
-				.add(toStylesheetString("src/resources/sudoku.css"));
 		stage.setScene(scene);
 		stage.show();
-	}
-
-	public String toStylesheetString(String stylesheet) {
-		try {
-			return new File(stylesheet).toURI()
-					.toURL()
-					.toString();
-		} catch (MalformedURLException e) {
-			return null;
-		}
 	}
 
 	public static void main(String[] args) {
